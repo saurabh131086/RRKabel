2c9521dc52cd5d169818ee82895557d6c5dba4ca
saurabh <saurabhpatelkri@gmail.com>
2022-02-18 16:20:39 +0530

    fixture for business enquiry

--- cypress/integration/HomePage/contact-us.js
+++ cypress/integration/HomePage/contact-us.js
@@ -3,38 +3,23 @@ import rgbHex from 'rgb-hex';
 
 
 describe("Test Contact Us form on HomePage", () => {
-  it.skip("Should be able to submit a successful submission via contact us form", () => {
+  it("Should be able to submit a successful submission via contact us form", function () {
     //cypress code
     cy.visit(Cypress.env('url'))
     cy.get('.fixed-button > img').click()
-    cy.get('#first_name').type('Test')
-    cy.get('#last_name').type('new')
-    cy.get(':nth-child(6) > .wpcf7-form-control-wrap > #email').type('test@gmail.com')
-    cy.get('#phone').type('9123456789')
-    cy.get('#select_type').select('Twin Parking System')
-    cy.get('#MainSelectCountry').select('Mexico')
-    cy.get('#MainSelectState').select('Jalisco')
-    cy.get('#MainSelectCity').select('Zula')
+    cy.get('#first_name').type(this.testdata.Firstname)
+    cy.get('#last_name').type(this.testdata.Lastname)
+    cy.get(':nth-child(6) > .wpcf7-form-control-wrap > #email').type(this.testdata.Email)
+    cy.get('#phone').type(this.testdata.phone)
+    cy.get('#select_type').select(this.testdata.Type)
+    cy.get('#MainSelectCountry').select(this.testdata.Country)
+    cy.get('#MainSelectState').select(this.testdata.State)
+    cy.get('#MainSelectCity').select(this.testdata.City)
     cy.wait(3000)
-    //  cy.get('#MainSelectCountry').select(0).should('have.value', 'India')
-    //cy.get('#MainSelectCountry').select('India').should ('have.value', 'India')
-     cy.get('#MainSelectCountry').select['India',1]
-    ///cy.get('#MainSelectCountry').then($country => {$country.val("India")}).click()   
-    // cy.wait(3000)
-    //cy.wait('#MainSelectCountry').its('Select Country').should('eq', 'India')
-    //   cy.get('#MainSelectState').then($State => {$State.val("Jalisco")})
-    //  //cy.get('#MainSelectCity').select('Pune')
-    //  cy.wait(3000)
-    //  cy.get('#MainSelectCity').then($city => {$city.val("Zula")})
-    //  cy.wait(3000)
     cy.get('#message').type('new')
     cy.get('#my-id > .uk-modal-dialog > .uk-modal-close-outside > svg').click()
-    // cy.get(':nth-child(13) > #submit').click()  
-
     cy.wait(2000)
-    // cy.get('#wpcf7-response-output wpcf7-display-none wpcf7-mail-sent-ok')
-    // .should('have.text', 'Thank you for your message. It has been sent.')
-    // cy.get('Contact us').xpath("//a[contains(@href, 'contact-us')]").click();
+   
   });
 
 
@@ -80,7 +65,17 @@ it('Should check Video on page', () => {
 
 
 
+before(function ()  {
 
+  cy.fixture('data').then(function (testdata) {
+    this.testdata = testdata
+})
+
+})
+
+
+
+})
 
 
 
@@ -93,4 +88,3 @@ it('Should check Video on page', () => {
 
 
 
-})
